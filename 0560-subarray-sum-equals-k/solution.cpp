914b0c0d541b23a5da639f3302b8d259ee4f692d
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-09-19 16:39:42 +0000

    Sync LeetCode submission Runtime - 33 ms (92.63%), Memory - 45.4 MB (67.72%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
 
-        unordered_map<long long, int> freq;
-        freq[0] = 1;  
-        long long sum = 0;
+        int n = arr.size();
+        unordered_map<int,int>mpp;
+        // Setting 0 in the map.
+        mpp[0]=1;
+        int preSum = 0;
         int count = 0;
+        for(int i=0 ;i<n ;i++){
 
-        for (int x : arr) {
-            sum += x;
-
-           
-            if (freq.find(sum - k) != freq.end()) {
-                count += freq[sum - k];
-            }
-
-            freq[sum]++; 
+            preSum +=arr[i];
+            // calculate s-k
+            int rem = preSum -k;
+            // Add the number of subarrays are remaining:
+            if(mpp.count(rem)==1) count += mpp[rem];
+            // Update the count of prefix sum  in the map
+            mpp[preSum]++;
         }
-
         return count;
     }
 };
