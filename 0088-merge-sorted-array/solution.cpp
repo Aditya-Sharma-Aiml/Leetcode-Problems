01d5592cd767a654ccf945ad7865b451b77bc302
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-09-27 14:10:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.2 MB (70.83%)

--- 0088-merge-sorted-array/solution.cpp
+++ 0088-merge-sorted-array/solution.cpp
@@ -4,8 +4,11 @@ public:
         int i = m-1;
         int j = n-1;
         int k = m+n-1;
+
         while(i>=0 && j>=0){
+
             if(nums1[i]>=nums2[j]){
+
                 nums1[k] = nums1[i];
                 i--;
             }
@@ -15,9 +18,14 @@ public:
             }
             k--;
         }
-         while(j >= 0) {
+        while(j >= 0) {
             nums1[k] = nums2[j];
             j--;
+            k--;
+        }
+        while(i >= 0) {
+            nums1[k] = nums1[i];
+            i--;
             k--;
         }
         
