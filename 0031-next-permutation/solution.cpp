2213d7defaa5e98b3d2f479c118ff42c0ab1038c
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-03 11:43:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.6 MB (67.81%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        
+        int n = nums.size();
+        int index = -1;
+
+        // Step 1: find break-point
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If no break-point found, reverse whole array
+        if (index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;  // important fix
+        }
+
+        // Step 2: find next greater element on right
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Step 3: reverse the suffix
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
+
