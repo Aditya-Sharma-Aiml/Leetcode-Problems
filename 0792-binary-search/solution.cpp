69df7ff10c67adfb506ef0534251c26c5a4db2f8
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-04 19:44:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.3 MB (79.93%)

---
+++ 0792-binary-search/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    private:
+        int binarySearch(vector<int>&arr, int low, int high, int target){
+
+            if( low > high ) return -1; // base case
+            int mid = low + (high - low)/2;
+
+            if(arr[mid] == target) return mid;
+
+            else if (target > arr[mid]){
+
+                return binarySearch(arr, mid+1, high, target);
+            } 
+
+            return binarySearch(arr, low, mid-1, target);  
+            
+        }
+    public:
+        int search(vector<int>& nums, int target) {
+            return binarySearch(nums, 0, nums.size()-1, target);
+        }
+};
