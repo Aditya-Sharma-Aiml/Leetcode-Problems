a42454126fe4b5ef41d6f6139edaf8ed63cd848b
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-06 21:21:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (32.96%)

---
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        
+        int n = arr.size();
+        int low = 0 , high = n-1;
+        
+        while(low <= high){
+
+            int mid = low + (high - low)/2;
+            
+            if(arr[mid] == target) return mid;
+
+            //identify sorted half
+            if(arr[low] <= arr[mid]){
+                //left sorted
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                // right sorted
+                if(arr[mid] <= target &&  target <= arr[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
