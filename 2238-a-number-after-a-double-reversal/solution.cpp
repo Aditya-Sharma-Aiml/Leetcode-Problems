6af74c4bcff5aca8f5465774b9dd783ec365da7a
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-09 22:25:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (57.22%)

---
+++ 2238-a-number-after-a-double-reversal/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int n){
+        int rev = 0;
+        while(n>0){
+            int digit = n % 10;
+            rev = rev *10 + digit;
+            n /= 10;
+        }
+        return rev;
+    }
+    bool isSameAfterReversals(int num) {
+        int rev1 = reverse(num);
+        int rev2 = reverse(rev1);
+        if (rev2==num) return true;
+        return false;
+    }
+};
