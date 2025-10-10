26072c805fcf9960119dadfe90b22bd8e7d3143e
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-10 22:22:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24.4 MB (74.10%)

---
+++ 3221-find-the-peaks/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        // NOTE : binary search se kroge toh bhi O(n) lagega
+        // kyunki tum har element ke liye check kroge na peak hai ya nhi
+        vector<int> peaks;
+        int n = mountain.size();
+
+        // check from 1 to n-2 (ignoring first and last)
+        for (int i = 1; i < n - 1; i++) {
+
+            if (mountain[i] > mountain[i - 1] &&
+                mountain[i] > mountain[i + 1]) {
+
+                peaks.push_back(i);
+            }
+        }
+        return peaks;
+    }
+};
+
