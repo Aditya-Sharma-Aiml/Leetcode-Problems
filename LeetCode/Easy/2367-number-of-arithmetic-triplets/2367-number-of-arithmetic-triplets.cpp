36de7fa111435d3169368d3ea3b598f1bda01cfe
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-22 19:59:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (28.4%) - LeetHub

---
+++ LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        unordered_set<int> seen;
+        int count = 0;
+        for (int num : nums) {
+            if (seen.count(num - diff) && seen.count(num - 2 * diff))
+                count++;
+            seen.insert(num);
+        }
+        return count;
+    }
+};
