872213eea61a25edbc3fc5ab7b61cd9ec9178667
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-11-26 00:17:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ LeetCode/Medium/2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -4,16 +4,14 @@ public:
         int i=0, j=0;
         int n = s.length();
         int m = t.length();
+
         while(i<m && j<n){
             if(t[i] == s[j]){
                 i++;
                 j++;
             }
-            else{
-                j++;
-            }
+            j++;
         }
-        if(i<m) return m-i;
-        return 0;
+        return m-i;   
     }
 };
