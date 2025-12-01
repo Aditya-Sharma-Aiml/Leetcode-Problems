8ec1f28f3483d1a1c70d521339c1338149a4f0ff
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-12-01 23:40:43 +0530

    Time: 35 ms (23.89%), Space: 73.1 MB (34.62%) - LeetHub

--- LeetCode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ LeetCode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
 
-        vector<int>inDegree(n+1,0);
-        vector<int>outDegree(n+1,0);
+        // vector<int>inDegree(n+1,0);
+        // vector<int>outDegree(n+1,0);
+        // for(auto t : trust){
+        //     outDegree[t[0]]++;
+        //     inDegree[t[1]]++;
+        // }
+        // for(int i=1 ; i<=n ; i++){
+        //     if(inDegree[i] == n-1 && outDegree[i] == 0) return i;
+        // }
+        // return -1;
+
+        // xxxxxxxxxxxxx xxxx xxxxxxxxxxxx
+        //outDegree -> never town judge
+        vector<int>inDeg(n+1,0);
         for(auto t : trust){
-            outDegree[t[0]]++;
-            inDegree[t[1]]++;
+            inDeg[t[1]]++;
+            inDeg[t[0]] = -1;
         }
         for(int i=1 ; i<=n ; i++){
-            if(inDegree[i] == n-1 && outDegree[i] == 0) return i;
+            if (inDeg[i] == n-1) return i ;
         }
         return -1;
+
     }
 };
