ef03a480d08ae2b99193334d4714cc214bc44b4d
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-12-10 23:03:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.11%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num < 2) return true;
+        long long x = num;
+        while (x * x > num)
+            x = (x + num / x) / 2;
+        return x * x == num;
+    }
+};
