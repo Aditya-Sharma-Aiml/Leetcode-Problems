fdec5a4b4736c36cb596cbbce7f0afe212d3bac1
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-12-31 00:31:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    int helper(string s , int i , int num , int sign){
+        int n = s.size();
+        if (i>=n || !isdigit(s[i])){
+            return (int)(sign*num);
+        }
+
+        num = num*10 + (s[i]-'0');
+
+        // if ans > INT_MAX : OVERFLOW OCCUR
+        if(i<n && (sign * num) > INT_MAX) return INT_MAX;
+        //if ans < INT_MIN : UNDERFLOW OCCUR
+        if(i<n && (sign * num) < INT_MIN ) return INT_MIN;
+
+        return helper(s,i+1,num,sign);
+    }
     int myAtoi(string s) {
         
         int n = s.length();
@@ -18,20 +33,24 @@ public:
         // if non-digit found(all are spaces) return 0
         if(i == n) return 0;
 
-        int ans = 0;
-        //3-> read all digit untill we get non-digit
-        while(i<n && isdigit(s[i])){
+        // // xxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxxxxxx
 
-            ans = ans * 10 + (s[i] - '0');
+        // long long ans = 0;
+        // //3-> read all digit untill we get non-digit
+        // while(i<n && isdigit(s[i])){
 
-            //4-> overflow/underflow:
-            //if ans > INT_MAX : OVERFLOW OCCUR
-            if(i<n && ans > INT_MAX) return INT_MAX;
-            //if ans < INT_MIN : UNDERFLOW OCCUR
-            if (i<n && ans < INT_MIN ) return INT_MIN;
+        //     ans = ans * 10 + (s[i] - '0');
 
-            i++;
-        }
-        return (sign * ans);
+        //     //4-> overflow/underflow:
+        //     //if ans > INT_MAX : OVERFLOW OCCUR
+        //     if(i<n && (sign * ans) > INT_MAX) return INT_MAX;
+        //     //if ans < INT_MIN : UNDERFLOW OCCUR
+        //     if (i<n && (sign * ans) < INT_MIN ) return INT_MIN;
+
+        //     i++;
+        // }
+        // return (int)(sign * ans);
+        // // xxxxxxxxxxx xxx xxxxxxxxxxxxxxxxxxxxx
+        return helper(s,i,0,sign);
     }
 };
