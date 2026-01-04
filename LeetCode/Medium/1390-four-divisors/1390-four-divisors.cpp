e048df68ad358334f75e9be57c10a52b58310398
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-01-05 00:43:14 +0530

    Time: 83 ms (25.62%), Space: 37.9 MB (9.85%) - LeetHub

---
+++ LeetCode/Medium/1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<int> hasDivisors(int n){
+        vector<int>divisors;
+
+        for(int i=1 ; i*i<=n ; i++){
+
+            if (n%i==0){
+                divisors.push_back(i);
+                if(i != n/i) divisors.push_back(n/i);
+            }
+        }
+        return divisors;
+    }
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int divSum = 0;
+        for(int num : nums){
+
+            vector<int> totalDiv = hasDivisors(num);
+        
+            if (totalDiv.size()==4) {
+                divSum+= accumulate(totalDiv.begin(),totalDiv.end(),0);
+            }
+        }
+        return divSum;
+    }
+};
