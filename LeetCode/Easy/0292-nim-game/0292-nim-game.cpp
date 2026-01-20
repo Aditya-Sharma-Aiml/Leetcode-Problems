4520ff26964aaf0c2e286946e7444910a4b230eb
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-01-20 19:33:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (15.09%) - LeetHub

--- LeetCode/Easy/0292-nim-game/0292-nim-game.cpp
+++ LeetCode/Easy/0292-nim-game/0292-nim-game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        return !(n%4==0);
+        return n%4 != 0; 
+        // 4 ya 4 ke multiple esa case hai ki mai jeet hi nhi skta  baki ke har case me jeet skta hun
+
+        //me   my friend 
+        // 1 -> 3
+        // 2 -> 2
+        // 3 -> 1
+        // last me my friend remove and he will win
     }
 };
