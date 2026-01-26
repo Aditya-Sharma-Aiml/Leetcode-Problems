3e4dd4060bcc2ee957e7848bb28ef4f931986483
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-01-27 00:20:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        vector<vector<int>>ans;
+        int minDiff = INT_MAX;
+
+        sort(arr.begin(), arr.end());
+
+        for(int i=1; i<arr.size(); i++){
+
+            int diff = arr[i] - arr[i-1];
+            if(diff < minDiff){
+                minDiff = diff;
+                ans.push_back({arr[i-1], arr[i]});
+            }
+            else if(diff == minDiff)
+                ans.push_back({arr[i-1], arr[i]});
+        }
+        return ans;
+    }
+};
