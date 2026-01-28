8c6f6ea947142a3e227d12c7fbb07a94fc17706e
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-01-28 23:23:40 +0530

    Time: 0 ms (100%), Space: 54.9 MB (98.36%) - LeetHub

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.cpp
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,14 +3,15 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         
         int n= nums.size();
-        // apply cycle sort but ignore the -ve when sort
-        // because ask is first missing +ve
-        // after sorting start searching from 1 -> n
-        // if number is not found then answer is n+1
+        // apply cycle sort but "ignore the -ve" when sort
+        // because ask is first missing +ve and "nums <= n"
+        // after sorting start searching from "1 -> n"
+        // if number is not found then answer is "n+1"
         int i=0;
         while(i<n){
-
-            if(nums[i] >0 && nums[i] <= n && nums[i] != nums[nums[i]-1]){
+            // use na kro wrna index : -ve ho gya toh overflow ho jayega
+            // int correctIndx = nums[i]-1; 
+            if(nums[i]>0 && nums[i] <= n && nums[i] != nums[nums[i]-1]){
                 swap(nums[i], nums[nums[i]-1]);
             }
             else i++;
