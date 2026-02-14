28cd3dec2e33120eb7124636cecdb1b4e422b3db
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-02-14 14:53:51 +0530

    Time: 0 ms (100%), Space: 11.1 MB (85.14%) - LeetHub

--- LeetCode/Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ LeetCode/Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -2,27 +2,39 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
 
-        // Dummy node to handle head swaps easily
-        ListNode dummy(0);
-        dummy.next = head;
+        // // Dummy node to handle head swaps easily
+        // ListNode dummy(0);
+        // dummy.next = head;
 
-        ListNode* prev = &dummy;
+        // ListNode* prev = &dummy;
 
-        while (prev->next && prev->next->next) {
+        // while (prev->next && prev->next->next) {
 
-            // Nodes to be swapped
-            ListNode* first = prev->next;
-            ListNode* second = prev->next->next;
+        //     // Nodes to be swapped
+        //     ListNode* first = prev->next;
+        //     ListNode* second = prev->next->next;
 
-            // Swapping
-            first->next = second->next;
-            second->next = first;
-            prev->next = second;
+        //     // Swapping
+        //     first->next = second->next;
+        //     second->next = first;
+        //     prev->next = second;
 
-            // Move prev forward
-            prev = first;
-        }
+        //     // Move prev forward
+        //     prev = first;
+        // }
 
-        return dummy.next;
+        // return dummy.next;
+
+        if(!head || !head->next) return head;
+
+        ListNode* second = head->next;
+
+        head->next = swapPairs(second->next);
+        
+        second->next = head;
+
+        return second;
+
+        
     }
 };
