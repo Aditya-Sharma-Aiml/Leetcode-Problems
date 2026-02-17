cc0c1020b49415e9931ec8df61b4b37e220b1f4e
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-02-17 20:05:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+
+        // target  = max banana kyunki increment hi allow hai
+        // nums = [1,2,4], k = 5
+        // ab  4 4 4 banana hai toh target = 4+4+4 = 4*3(max*window size) 
+        // okay now 1+2+4 = 7 already hai toh isko ghata do 
+        // required = target - curr_Sum
+
+        // formula : required = nums[i] * window_size - currSum
+
+        int l = 0, r = 0;
+        int ans = 1;
+
+        sort(nums.begin(), nums.end());
+
+        long long currSum = 0 ;
+        
+        while(r<nums.size()){
+
+            currSum += nums[r];
+
+            long long required = nums[r] * (r-l+1) - currSum;
+
+            if(required > k ){
+
+                currSum -= nums[l];
+                l++;
+            }
+
+            ans  = max(ans , r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
