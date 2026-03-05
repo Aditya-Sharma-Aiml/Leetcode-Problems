e38fbbcb8eddde4578f1a3a39845c43eb3deb73a
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-03-06 01:06:44 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.48%) - LeetHub

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        
         int n = height.size();
-
-        vector<int>prefixMax(n);
-        vector<int>suffixMax(n);
-
-        //prefix max of each element
-        prefixMax[0] = height[0];
-        for(int i=1; i<n ; i++){
-
-            prefixMax[i] = max(prefixMax[i-1], height[i]);
-        }
-
-        //suffix max of each element
-        suffixMax[n-1] = height[n-1];
-        for(int i=n-2; i>=0 ; i--){
-
-            suffixMax[i] = max(suffixMax[i+1], height[i]);
-        }
-
-        // calculate total unit of water trapped
+        int l = 0, r = n-1;
         int total = 0;
-        for(int  i=0; i<n; i++){
+        int leftMax = 0, rightMax = 0;
 
-            int leftMax = prefixMax[i]; // curr prefix max
-            int rightMax = suffixMax[i]; // curr suffix max
+        while(l<=r){
 
-            total += min(leftMax, rightMax) - height[i];
-            
+            if(height[l] < height[r]){
+
+                if(height[l] < leftMax){
+
+                    total += leftMax - height[l] ;
+                }
+                else{
+                    leftMax = height[l];
+                }
+                l++;
+
+            }
+            else{
+                if(height[r] < rightMax){
+
+                    total += rightMax - height[r] ;
+                }
+                else{
+                    rightMax = height[r];
+                }
+                r--;
+
+            }
         }
         return total;
     }
