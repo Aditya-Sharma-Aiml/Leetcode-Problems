8796a7ffcaed4edb5d3b7231f55aa6e78a0b135f
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2026-03-09 00:38:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
+++ LeetCode/Medium/3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int smallestBalancedIndex(vector<int>& nums) {
-        // log(a∗b∗c)=loga+logb+logc  kyonki nums[i] <= 1e9
+        
+        int n  = nums.size();
+        long long sum = accumulate(nums.begin(), nums.end(), 0LL)
+         - nums[n-1];
+        long long prod = 1;
+        if(prod == sum) return n-1;     
+        for(int i=n-2 ; i>=0 ; i--){
 
-        int n = nums.size();
-        vector<double>suffixLog(n+1, 0);
-
-        for(int i = n-1 ; i>=0; i--){
-            suffixLog[i] = suffixLog[i+1] + log((double)nums[i]);
-        }
-
-        long long  prefixsum = 0;
-
-        for(int i=0; i<n ; i++){
-
-            double rightlog = suffixLog[i+1];
-
-            if(prefixsum >  0){
-                //floating absolute value.
-                if(fabs(log((double)prefixsum) - rightlog) < 1e-9)
-                    return i;
-            }
-            prefixsum += nums[i];
-
-        }
-        return -1;
-
+            prod *= nums[i+1];
+            sum -= nums[i];
+            if(sum == prod) return i;
+            else if(sum < prod) break;
+        }  
+        return -1; 
     }
 };
